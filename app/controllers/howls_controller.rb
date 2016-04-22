f56fbf29648e8ed454a5b3d5af2b7d73cee102b7
Patrick <patrickwiechert@yahoo.com>
2016-04-22 10:47:43 -0500

    Added in views and controllers

--- app/controllers/howls_controller.rb
+++ app/controllers/howls_controller.rb
@@ -1,1 +1,25 @@
-# Howl Controller
+# Howls Controller
+
+# NEW
+get "/howls/new" do
+  @howl = Howl.new
+  erb :"howls/new"
+end
+
+
+# CREATE
+post "/howls" do
+  @howl = Howl.new(params)
+  if @howl.save
+    redirect to("/howls")
+  else
+    erb :"howls/new"
+  end
+end
+
+
+# INDEX
+get "/howls" do
+  @howls = Howl.order(:name)
+  erb :"howls/index"
+end
--- app/controllers/wolves_controller.rb
+++ app/controllers/wolves_controller.rb
@@ -1,1 +1,25 @@
-# Wolves Controller 
+# Wolves Controller
+
+# NEW
+get "/wolves/new" do
+  @wolf = Wolf.new
+  erb :"wolves/new"
+end
+
+
+# CREATE
+post "/wolves" do
+  @wolf = Wolf.new(params)
+  if @wolf.save
+    redirect to("/wolves")
+  else
+    erb :"wolves/new"
+  end
+end
+
+
+# INDEX
+get "/wolves" do
+  @wolves = Wolf.order(:name)
+  erb :"wolves/index"
+end
